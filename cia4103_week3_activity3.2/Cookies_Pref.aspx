ca27d8f80d649bb676bfc80aba48a64a15261ddf
Its Me <overwatcheddude@gmail.com>
2018-09-18 12:52:37 +0400

    first commit

---
+++ cia4103_week3_activity3.2/Cookies_Pref.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Cookies_Pref.aspx.cs" Inherits="Cookies_Pref" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body id="PageBody" runat="server">
+    <h1>
+        Activity 3.2 - Using Cookies
+    </h1>
+    <form id="form1" runat="server">
+        <div>
+            Select your choice of color for page background:<asp:RadioButtonList ID="rblColor" runat="server">
+                <asp:ListItem Selected="True">White</asp:ListItem>
+                <asp:ListItem>Pink</asp:ListItem>
+                <asp:ListItem>Grey</asp:ListItem>
+            </asp:RadioButtonList>
+            Select your favourite font:<asp:RadioButtonList ID="rblFont" runat="server">
+                <asp:ListItem Selected="True">Arial</asp:ListItem>
+                <asp:ListItem>Tahoma</asp:ListItem>
+                <asp:ListItem>Verdana</asp:ListItem>
+            </asp:RadioButtonList>
+            <asp:Button ID="btnSubmit" runat="server" OnClick="btnSubmit_Click" Text="Submit" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ cia4103_week3_activity3.2/Cookies_Pref.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Cookies_Pref : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //Check if cookie exists, if not then create one with an expiration date of 1 hour.
+        if (Request.Cookies["colorCookie"] == null)
+        {
+            HttpCookie colorCookie = new HttpCookie("colorCookie");
+            colorCookie.Expires = DateTime.Today.AddHours(1);
+            Response.Cookies.Add(colorCookie);
+        }
+        if (Request.Cookies["fontCookie"] == null)
+        {
+            HttpCookie fontCookie = new HttpCookie("fontCookie");
+            fontCookie.Expires = DateTime.Today.AddHours(1);
+            Response.Cookies.Add(fontCookie);
+        }
+
+        //If the color cookie exists, then set color.
+        if (Request.Cookies["colorCookie"] != null)
+        {
+            PageBody.Attributes["bgcolor"] = "red";
+        }
+    }
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        
+    }
+}
